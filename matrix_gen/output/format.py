4f8fb3363e76fa3ce36076170d53538917695421
Yaroslav Borysiuk <Yaroslav.Borysiuk@lifecell.com.ua>
2021-03-14 11:14:34 +0200

    Indent removed

--- matrix_gen/output/format.py
+++ matrix_gen/output/format.py
@@ -7,16 +7,15 @@ from matrix_sum.output.format import ABCFormat, ABCFormatFactory
 
 
 class MatrixPairJsonFormat(ABCFormat):
-    def __init__(self, matrix1: ABCMinimalMatrix, matrix2: ABCMinimalMatrix, indent: int = 2) -> None:
+    def __init__(self, matrix1: ABCMinimalMatrix, matrix2: ABCMinimalMatrix) -> None:
         self._matrix1 = matrix1
         self._matrix2 = matrix2
-        self._indend = indent
     
     def __call__(self) -> str:
         to_list1 = ToList(matrix=self._matrix1)
         to_list2 = ToList(matrix=self._matrix2)
         result = {"a": to_list1(), "b": to_list2()}
-        return dumps(result, indent=self._indend)
+        return dumps(result)
 
 
 class MatrixPairJsonFormatFactory(ABCFormatFactory):
